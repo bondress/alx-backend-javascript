3439d62be920be1d245dd4fbcd4fe208edca72da
Laide Endeley <endeleylaide@gmail.com>
2024-02-05 20:19:57 -0800

    Added solutions to all tasks

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,5 @@
+export default function appendToEachArrayValue(array, appendString) {
+  const myarray = [];
+  for (const item of array) {
+    myarray.push(appendString + item);
+  }
---
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,13 @@
+export default function iterateThroughObject(reportWithIterator) {
+  let res = '';
+
+  for (const [index, item] of Object.entries(reportWithIterator)) {
+    res += `${item}`;
+
+    if (parseInt(index) !== reportWithIterator.length - 1) {
+      res += ' | ';
+    }
+  }
+
+  return res;
+}
